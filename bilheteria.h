e8caa4f4067dbffc4a454a649c125331c7ac09a6
Lucas Montino <lucasmontino@hotmail.com>
2023-10-22 03:33:04 -0300

    Adiciona arquivos essenciais para o funcionamento

--- bilheteria.h
+++ bilheteria.h
@@ -3,20 +3,21 @@
 
 #define MAX_BILHETES 50
 
-void venda_bilhete();
-void pagamento_bilhete();
-void gerarCod_bilhete();
+void escolhaTema();
+void dadosUsuario();
+void pagamento();
+void gerarCod();
 
 extern char temasDoUsuario[MAX_BILHETES][40], nomeUsuario[30], codigo[5];
-extern int numCompras, idadeUsuario, estudante, temaEscolhido;
+extern int numCompras, idadeUsuario, estudante, temaEscolhido, pagamentoRealizado;
 
 struct Registros{
         int numBilhete;
         char    nome[50];
         int     idade;
-        char    tema[MAX_BILHETES][50];
-        char    codigoBilhete[MAX_BILHETES][5];
-        char    status[MAX_BILHETES][10];
+        char    tema[50];
+        char    codigoBilhete[5];
+        char    status[10];
     };
     
 struct Registros ingressos[MAX_BILHETES];
